9b7b7946c2c65e728813b8124310a10e7e03219a
Acer - Kemal <kemal.satya.wibowo1@gmail.com>
2026-02-05 12:01:00 +0700

    pembuatan frontend login/submit

--- backend/public/script.js
+++ backend/public/script.js
@@ -1,56 +1,46 @@
-let form = document.getElementById("form");
-let inputNamaBarang = document.getElementById("barang");
-let inputJumlahBarang = document.getElementById("jumlah");
-let inputJenisPembayaran = document.getElementById("pembayaran");
+let formPage = document.querySelector(".form");
 
-let inputTambahan = document.getElementById("input-tambahan");
+//// alternate condition
+let optionWrapper = document.querySelector(".option-wrapper");
+let optionRadios = optionWrapper.querySelectorAll('input[type="radio"]');
+let inputAffected = document.getElementById("input-affected");
+let buttonAffected = document.getElementById("button-affected");
 
-// pembayaran barang
-let akhir_jenisPembayaran;
-inputJenisPembayaran.addEventListener("change", () => {
-  akhir_jenisPembayaran = inputJenisPembayaran.value;
-  inputTambahan.innerHTML = "";
-
-  if (akhir_jenisPembayaran == "transfer" || akhir_jenisPembayaran == "qris") {
-    inputTambahan.innerHTML = `
-      <label for="bukti_pembayaran">Bukti : </label><br>
-      <input id="bukti_pembayaran" type="file" accept="image/*" />
-    `;
-  }
+// signup condition
+let optionChecked;
+optionRadios.forEach((input) => {
+  input.addEventListener("click", (e) => {
+    optionChecked = e.target;
+    if (optionChecked.value === "signup") {
+      inputAffected.innerHTML = `
+        <input
+            type="password"
+            name="passwordConfirmation"
+            id="passwordConfirmation"
+            class="main-input"
+            placeholder="Password Confirmation"
+        />
+        `;
+      // other affected condition
+      buttonAffected.value = `Sign Up`;
+    } else if (optionChecked.value === "login") {
+      inputAffected.innerHTML = ``;
+      buttonAffected.value = `Login`;
+    }
+  });
 });
 
-// submit order
-form.addEventListener("submit", (e) => {
+//// Password Confirmation
+formPage.addEventListener("submit", (e) => {
   e.preventDefault();
-  const formData = new FormData(form);
-  const data = Object.fromEntries(formData);
-
-  fetch("/submitPemesanan", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  })
-    .then((response) => {
-      if (!response.ok) {
-        // Jika respons tidak berhasil (misalnya status HTTP 4xx atau 5xx)
-        // Kita bisa mencoba membaca pesan error dari server jika ada
-        return response.json().then((err) => {
-          throw err;
-        });
-      }
-      return response.json(); // Mengasumsikan server mengembalikan JSON
-    })
-    .then((data) => {
-      console.log("Success:", data);
-      alert("Pemesanan berhasil disimpan!"); // Beri tahu pengguna bahwa operasi berhasil
-      form.reset(); // Opsional: reset formulir setelah sukses
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-      alert(
-        `Terjadi kesalahan saat menyimpan pemesanan: ${error.message || error}`,
-      ); // Beri tahu pengguna tentang kesalahan
-    });
+  let submitData = new FormData(formPage);
+  // cara kelola data
+  let data = {};
+  for (let [key, value] of submitData.entries()) {
+    data[key] = value;
+  }
+  // cara kelola data yang salah
+  // let data = Object.entries(submitData);
+  let sendData = JSON.stringify(data);
+  // console.log(sendData);
 });
